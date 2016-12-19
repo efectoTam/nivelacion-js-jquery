13b8d7c12f9348b2bd43f922fc30a410034b5556
efectoTam <tam.luque@gmail.com>
2016-12-19 12:14:33 -0300

    Etapa 8: La clase make desparece o aparece según en qué botón se haga click

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -1,4 +1,13 @@
 $(document).ready( function(){
 	$(".js-menu").hide();
 	console.log("El documento está listo");
+	$(".js-show-recipe").click(function() {
+  		alert("Recipe!");
+  		$(".page").removeClass("make");
+	});
+	$(".js-show-make").click(function() {
+  		alert("Make it!");
+  		$(".make").toggle();
+  		$(".page").addClass("make");
+	});
 });
